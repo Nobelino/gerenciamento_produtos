e1277b0df869caf097697e7b3d86d847cedefbfd
Gabriel Davi <146792882+Nobelino@users.noreply.github.com>
2024-11-17 19:53:27 -0300

    Update Category.js
    Comentários adicionados.

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-  name: {
+  name: { // Nome da categoria.
     type: String,
     required: true
-  },
-  description: {
+  }, 
+  description: { // Descrição da categoria.
     type: String,
     required: true
   }
-}, { timestamps: true });
+}, { timestamps: true }); // Adiciona data de criação e atualização automaticamente.
 
 module.exports = mongoose.model('Category', categorySchema);
