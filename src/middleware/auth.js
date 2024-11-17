dcbcd0075420d3691d062f4a65609f5576b85e2e
Gabriel Davi <146792882+Nobelino@users.noreply.github.com>
2024-11-17 19:57:23 -0300

    Update auth.js
    Comentários adicionados.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, 'your_jwt_secret');
-    req.userId = decoded.userId;
-    next();
+    const token = req.header('Authorization').replace('Bearer ', ''); // Lê o token JWT.
+    const decoded = jwt.verify(token, 'your_jwt_secret'); // Valida o token.
+    req.userId = decoded.userId; // Armazena o ID do usuário no `req`.
+    next(); // Permite o acesso à rota.
   } catch (error) {
-    res.status(401).json({ error: 'Por favor, autentique-se.' });
+    res.status(401).json({ error: 'Por favor, autentique-se.' }); // Se falhar, retorna erro.
   }
 };
 
